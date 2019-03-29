0e3918757b2654fa8d64a95dfcd92c2c09f088fe
laura-iutifs <laurascelles18@gmail.com>
2019-03-29 15:56:20 +0100

    regerzg

--- TD4/app/models/project.js
+++ TD4/app/models/project.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   name:DS.attr('string'),
   descriptif:DS.attr('string'),
   creationDate:DS.attr('date'),
-  duoDate:DS.attr('date'),
+  dueDate:DS.attr('date'),
   owner:DS.belongsTo('developer'),
   stories:DS.hasMany('story', {inverse:'project'})
 });
