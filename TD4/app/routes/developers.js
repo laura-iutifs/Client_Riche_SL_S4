4e61667efe67948db19949ad69c95d6de0e00c55
Laura <21701944@etu.unicaen.fr>
2019-04-05 13:20:26 +0200

    modif /routes

--- TD4/app/routes/developers.js
+++ TD4/app/routes/developers.js
@@ -3,5 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(){
     return this.store.findAll('developer');
+  },
+  actions:{
+    delete(dev){
+      dev.destroyRecord();
+    }
   }
 });
--- TD4/app/routes/projects/new.js
+++ TD4/app/routes/projects/new.js
@@ -2,11 +2,12 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(){
-    return {copy:{}};
+    return {copy:{} };
   },
 
   actions:{
     save(projects){
+      debugger
     //  let dev=model.copy;
       projects=this.store.createRecord("project", projects.copy);
       projects.save().then(this.transitionTo("projects"));
